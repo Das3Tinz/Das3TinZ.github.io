c66a81ca9e719bd9da7c610dd26d90abf5ad6295
das3tins <136376125+Das3Tinz@users.noreply.github.com>
2023-06-13 20:15:05 +0400

    web

---
+++ web/standalone/MySQL_config.php
@@ -0,0 +1,9 @@
+<?php
+$dbname = 's1242_dynmap';
+$dbhost = '31.132.1.60';
+$dbport = 3306;
+$dbuserid = 'u1242_F3bMelgjPx';
+$dbpassword = '+GJu+k!kjCc4186bsb5FCqK6';
+$dbprefix = '';
+$loginenabled = false;
+?>
